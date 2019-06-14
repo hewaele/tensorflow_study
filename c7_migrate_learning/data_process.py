6ad7bd6aae6e44f06748570246f4df67c3993b65
hewaele <hewaele@qq.com>
2019-06-14 11:07:19 +0800

    2019年06月14日11:07:25

--- c7_migrate_learning/data_process.py
+++ c7_migrate_learning/data_process.py
@@ -1,4 +1,4 @@
-#使用npz格式数据集很大，效果不好
+#使用npz格式在大数据集时，读取速度满效果不好
 import numpy as np
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
@@ -14,33 +14,40 @@ photos_extentions = ['.jpg', '.JPG', '.jpeg', '.JPEG']
 data_path = './data/flower_photos/'
 count = 0
 
-with tf.Session() as sess:
+def creat_npy():
     for dir in os.listdir(data_path):
         if os.path.isdir(os.path.join(data_path, dir)):
             for image in os.listdir(os.path.join(data_path, dir)):
-                img = tf.gfile.FastGFile(os.path.join(data_path + dir, image), 'rb').read()
-                image_data = tf.image.decode_jpeg(img)
-                #转换图片类型
-                if image_data.dtype != tf.float32:
-                    image_data = tf.image.convert_image_dtype(image_data, tf.float32)
-                #resize图片299*299  双线性插值
-                image_data = tf.image.resize_images(image_data, [299, 299])
-                data_image.append(sess.run(image_data))
+                # img = tf.gfile.FastGFile(os.path.join(data_path + dir, image), 'rb').read()
+                # image_data = tf.image.decode_jpeg(img)
+                # #转换图片类型
+                # if image_data.dtype != tf.float32:
+                #     image_data = tf.image.convert_image_dtype(image_data, tf.float32)
+                # #resize图片299*299  双线性插值
+                # image_data = tf.image.resize_images(image_data, [299, 299])
+                image_data = Image.open(os.path.join(data_path+dir, image))
+                image_data = image_data.resize((299, 299))
+
+                data_image.append(np.array(image_data))
                 data_label.append(dir)
-                plt.imshow(image_data.eval())
-                plt.show()
-            break
-        break
 
 
 
-#将数据进行随机切分
-train_x, test_x, train_y, test_y = train_test_split(data_image, data_label, test_size=0.2)
+    #将数据进行随机切分
+    train_x, test_x, train_y, test_y = train_test_split(data_image, data_label, test_size=0.2)
 
-print("{} {} {} {}".format(len(train_x), len(train_y), len(test_x), len(test_y)))
-#将数据保存
-train_valid_data = np.asarray([train_x, train_y, test_x, test_y])
-np.save('./data/train_valid_data.npy', train_valid_data)
-sess.close()
-print('done')
+    print("{} {} {} {}".format(len(train_x), len(train_y), len(test_x), len(test_y)))
+    #将数据保存
+    train_valid_data = np.asarray([train_x, train_y, test_x, test_y])
+    np.save('./data/train_valid_data.npy', train_valid_data)
+    print('done')
 
+def load_npy():
+    data = np.load('./data/train_valid_data.npy', allow_pickle=True)
+    train_x, test_x, train_y, test_y = data[0], data[1], data[2], data[3]
+    print(len(train_y))
+    print(len(train_x))
+
+if __name__ == '__main__':
+    # creat_npy()
+    load_npy()
--- c7_migrate_learning/make_tfrecord.py
+++ c7_migrate_learning/make_tfrecord.py
@@ -1,25 +1,29 @@
 import tensorflow as tf
 import numpy as np
+from PIL import Image
 import os
 
 labels_dic = {'daisy':0, 'dandelion':1, 'roses':2, 'sunflowers':3, 'tulips':4}
 
 
 def creat_tfrecord(file_path, out_path):
+    count = 0
     with tf.Session() as sess:
         #定义一个tfrecord wirter
         writer = tf.python_io.TFRecordWriter(out_path)
         for dir in os.listdir(file_path):
             if os.path.isdir(os.path.join(file_path, dir)):
                 for img in os.listdir(os.path.join(file_path, dir)):
-                    img_data = tf.gfile.FastGFile(os.path.join(file_path+dir, img), 'rb').read()
-                    img_data = tf.image.decode_jpeg(img_data)
-                    img_data = tf.image.resize_images(img_data, [299, 299])
+                    # img_data = tf.gfile.FastGFile(os.path.join(file_path+dir, img), 'rb').read()
+                    # img_data = tf.image.decode_jpeg(img_data)
+                    # img_data = tf.image.resize_images(img_data, [299, 299])
+                    img_data = Image.open(os.path.join(file_path+dir, img))
+                    img_data = img_data.resize((299, 299)).tobytes()
 
                     #创建一个example
                     feature = tf.train.Features(feature={
-                        'img':bytes_feature(bytes(img_data.eval())),
-                        'label':int64_feature(labels_dic[dir])
+                        # 'img':bytes_feature(img_data),
+                        'label': int64_feature(labels_dic[dir])
                     })
                     example = tf.train.Example(features=feature)
                     #将该张图片写入
@@ -46,19 +50,25 @@ def load_tfrecord(tfrecord_path):
 
     #从文件队列中对出一个样本
     _, example = reader.read(file_queue)
-
+    #
+    print('test')
     #解析一个样本
     features = tf.parse_single_example(example,
                                        features={
                                            'img':tf.FixedLenFeature([], tf.string),
                                            'label': tf.FixedLenFeature([], tf.int64)})
-
+    print('test')
     #将解析出来的数据重新转换为原始数据
-    image = tf.decode_raw(features['img'], tf.uint8)
+    image = tf.reshape(tf.decode_raw(features['img'], tf.uint8), [299, 299, 3])
     label = tf.cast(features['label'], tf.int32)
-
     with tf.Session() as sess:
-        print(sess.run(image))
+        #启动队列线程
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(coord=coord)
+        val = sess.run(image)
+        print(len(val[0][0]))
+    return image, label
+
 
 def datasets_tfrecord(tfrecord_path):
     datasets = tf.data.TFRecordDataset([tfrecord_path])
@@ -67,9 +77,44 @@ def datasets_tfrecord(tfrecord_path):
 def parse_function(example):
     pass
 
+def tfrecord_test():
+    tf_path = './data/tf_test.tfrecord'
+    #随机生成一组数据用于测试
+    a = [i for i in range(10)]
+
+    #写入
+    '''
+    创建一个writer 
+    循环数据
+    穿件一个字典对象
+    创建一个example实例，将数据字典传入
+    将example写入
+    '''
+    writer = tf.python_io.TFRecordWriter(tf_path)
+    for i in a:
+        features = tf.train.Features(feature={'label':tf.train.Feature(int64_list=tf.train.Int64List(value=[i]))})
+        example = tf.train.Example(features=features).SerializeToString()
+        writer.write(example)
+
+    writer.close()
+
+    #读取
+    reader = tf.TFRecordReader()
+    sqeue = tf.train.string_input_producer([tf_path])
+    _, example = reader.read(sqeue)
+    features = tf.parse_single_example(example,features={'label':tf.FixedLenFeature([], tf.int64)})
+    label = tf.cast(features['label'], tf.uint8)
+    print(label)
+
+    with tf.Session() as sess:
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(coord=coord)
+        for i in range(10):
+            print(sess.run(label))
 
 if __name__ == "__main__":
     file_path = './data/flower_photos/'
     out_path = './data/flowers.tfrecord'
     # creat_tfrecord(file_path, out_path)
-    load_tfrecord(out_path)
+    image, label = load_tfrecord(out_path)
+    tfrecord_test()
